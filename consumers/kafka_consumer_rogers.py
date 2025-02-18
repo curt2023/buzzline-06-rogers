44604dca5a98856a9c8867d974aeb389cd792f44
Curtis Rogers <ctrogers87@gmail.com>
2025-02-18 11:25:11 -0600

    Finally got my visual updating appropriatly. I am going to make another visual to display

--- consumers/kafka_consumer_rogers.py
+++ consumers/kafka_consumer_rogers.py
@@ -47,11 +47,11 @@ from utils.utils_producer import verify_services, is_topic_available
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from consumers.db_sqlite_rogers import init_db, insert_message
 
-
-
 fig, ax = plt.subplots()
 fig.patch.set_facecolor('slategrey')
 plt.ion()
+
+
 DB_PATH = config.get_sqlite_path()
 
 def fetch_data():
@@ -67,25 +67,25 @@ def fetch_data():
 
   
 def update_chart():
-    visual_data1 = fetch_data() 
+    while True:
+
+        visual_data1 = fetch_data() 
  
-    ax.clear() #clear previous chart
+        ax.clear() #clear previous chart
 
-    
-
-    genre, avg_sentiment = zip(*visual_data1)
+        genre, avg_sentiment = zip(*visual_data1)
 
 
-    ax.bar(genre, avg_sentiment, color="lawngreen", edgecolor ='orange')
-    ax.set_title("Average Sentiment per Category")
-    ax.set_ylabel("avg_sentiment")
-    ax.set_xlabel("genre")
-    ax.set_facecolor("lightsteelblue")
-    ax.set_ylim(0,1)
+        ax.bar(genre, avg_sentiment, color="lawngreen", edgecolor ='orange')
+        ax.set_title("Average Sentiment per Category")
+        ax.set_ylabel("avg_sentiment")
+        ax.set_xlabel("genre")
+        ax.set_facecolor("lightsteelblue")
+        ax.set_ylim(0,1)
 
-    plt.tight_layout()
-    plt.draw()
-    plt.pause(0.5)
+        plt.tight_layout()
+        plt.draw()
+        plt.pause(2)
 
    
 
@@ -118,10 +118,11 @@ def process_message(message: dict) -> None:
         }
         logger.info(f"Processed message: {processed_message}")
         return processed_message
+        #insert_message(processed_message, DB_PATH)
     except Exception as e:
         logger.error(f"Error processing message: {e}")
         return None
-    update_chart()
+    
 
 
 #####################################
@@ -132,9 +133,7 @@ def process_message(message: dict) -> None:
 def consume_messages_from_kafka(
     topic: str,
     kafka_url: str,
-    group: str,
-    sql_path: pathlib.Path,
-    interval_secs: int,
+    group: str
 ):
     """
     Consume new messages from Kafka topic and process them.
@@ -151,17 +150,7 @@ def consume_messages_from_kafka(
     logger.info(f"   {topic=}")
     logger.info(f"   {kafka_url=}")
     logger.info(f"   {group=}")
-    logger.info(f"   {sql_path=}")
-    logger.info(f"   {interval_secs=}")
 
-    logger.info("Step 1. Verify Kafka Services.")
-    try:
-        verify_services()
-    except Exception as e:
-       logger.error(f"ERROR: Kafka services verification failed: {e}")
-       sys.exit(11)
-
-    logger.info("Step 2. Create a Kafka consumer.")
     try:
         consumer: KafkaConsumer = create_kafka_consumer(
             topic,
@@ -172,36 +161,21 @@ def consume_messages_from_kafka(
         logger.error(f"ERROR: Could not create Kafka consumer: {e}")
         sys.exit(11)
 
-    logger.info("Step 3. Verify topic exists.")
-    if consumer is not None:
-        try:
-            is_topic_available(topic)
-            logger.info(f"Kafka topic '{topic}' is ready.")
-        except Exception as e:
-            logger.error(
-                f"ERROR: Topic '{topic}' does not exist. Please run the Kafka producer. : {e}"
-            )
-            sys.exit(13)
-
-    logger.info("Step 4. Process messages.")
-
     if consumer is None:
         logger.error("ERROR: Consumer is None. Exiting.")
         sys.exit(13)
 
     try:
-        # consumer is a KafkaConsumer
-        # message is a kafka.consumer.fetcher.ConsumerRecord
-        # message.value is a Python dictionary
         for message in consumer:
             processed_message = process_message(message.value)
             if processed_message:
-                insert_message(processed_message, sql_path)
-
+                insert_message(processed_message, DB_PATH)
+    
+    except KeyboardInterrupt:
+        logger.warning("Consumer interrupted by user")
     except Exception as e:
         logger.error(f"ERROR: Could not consume messages from Kafka: {e}")
         raise
-
 
 #####################################
 # Define Main Function
@@ -220,48 +194,19 @@ def main():
 
     logger.info("STEP 1. Read environment variables using new config functions.")
 
-
-
     try:
         topic = config.get_kafka_topic()
         kafka_url = config.get_kafka_broker_address()
         group_id = config.get_kafka_consumer_group_id()
-        interval_secs: int = config.get_message_interval_seconds_as_int()
-        sqlite_path: pathlib.Path = config.get_sqlite_path()
-        logger.info("SUCCESS: Read environment variables.")
 
-    except Exception as e:
-        logger.error(f"ERROR: Failed to read environment variables: {e}")
-        sys.exit(1)
-
-    logger.info("STEP 2. Delete any prior database file for a fresh start.")
-    if sqlite_path.exists():
-        try:
-            sqlite_path.unlink()
-            logger.info("SUCCESS: Deleted database file.")
-        except Exception as e:
-            logger.error(f"ERROR: Failed to delete DB file: {e}")
-            sys.exit(2)
-
-
-    logger.info("STEP 3. Initialize a new database with an empty table.")
-    
-
-
-    try:
         init_db(DB_PATH)
-    except Exception as e:
-        logger.error(f"ERROR: Failed to create db table: {e}")
-        sys.exit(3)
 
-    logger.info("STEP 4. Begin consuming and storing messages.")
-    try:
-        consume_messages_from_kafka(
-            topic, kafka_url, group_id, sqlite_path, interval_secs
-        )
+        import threading
+        consumer_thread = threading.Thread(target= consume_messages_from_kafka, args=(topic,kafka_url,group_id))
+        consumer_thread.daemon = True
+        consumer_thread.start()
 
-    except Exception as e:
-        logger.error (f"ERROR: Failed to store messages.")
+        update_chart()
 
     except KeyboardInterrupt:
         logger.warning("Consumer interrupted by user.")
@@ -270,13 +215,13 @@ def main():
     finally:
         logger.info("Consumer shutting down.")
 
-
+   
 
 #####################################
 # Conditional Execution
 #####################################
 
-update_chart()
+
 if __name__ == "__main__":
     main()
 
